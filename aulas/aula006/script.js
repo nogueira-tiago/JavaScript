98558551f22d53b4b80cae9c9b8972d419992373
nogueira-tiago <alchemist212@hotmail.com>
2023-03-13 14:12:40 -0400

    aprendendo JS

---
+++ aulas/aula006/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var imagem = window.document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    if (hora >= 5 && hora< 12){
+        imagem.src = 'imagens/fotomanha.JPG' 
+        document.body.style.background = '#ffef87'
+    } else if(hora >=12 && hora < 19){
+        imagem.src = 'imagens/fototarde.JPG'
+        document.body.style.background = '#54deff'
+    } else {
+        imagem.src = 'imagens/fotonoite.JPG'
+        document.body.style.background = '#0c0e35'
+    }
+}
